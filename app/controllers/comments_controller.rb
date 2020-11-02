29cfbc84d8600b10437bb74cceede05abce395cc
yamamokou3 <alsk.89vbm25@gmail.com>
2020-11-02 20:15:20 +0900

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
     else  
       @prototype = @comment.prototype
       @comments = @prototype.comments
+      
       render "prototypes/show"
     end
   end
